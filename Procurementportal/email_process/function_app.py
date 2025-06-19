44d1c3cbb9c7fb01dace9d92d6347c2ba64c4e0f
divya17-dev <divya.mehra@itradiant.com>
2025-06-19 18:30:03 +0530

    Add files via upload

---
+++ Procurementportal/email_process/function_app.py
@@ -0,0 +1,18 @@
+import logging
+import azure.functions as func
+import traceback
+
+# Fix import 
+try:
+    from .extraction import start_email_monitoring
+except ImportError:
+    from email_process.extraction import start_email_monitoring  
+
+def main(mytimer: func.TimerRequest) -> None:
+    logging.info("Timer trigger executed.")
+    
+    try:
+        start_email_monitoring()  
+    except Exception as e:
+        logging.error(f"Error during email monitoring: {e}")
+        logging.error("Traceback:\n" + traceback.format_exc())
